8a9d8e49c69c3f00d6e181874f3f8b1cefaf899f
AAMinoldo <minoldoaldo@gmail.com>
2025-03-27 19:59:10 -0300

    subo los cambio con las nva rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Python_link_bio",
-    api_url="https://pruebareflex-production.up.railway.app:8000"
+    api_url="https://pruebareflex-production.up.railway.app:8000",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://aldominoldopruebareflex.vercel.app/"
+    ]
 )
